0d0421fd41deb7ec95cf7a1f6e0210cb01ffa958
parth299 <sainiparth299@gmail.com>
2025-05-18 20:44:59 +0530

    chatroom.hpp header file complete and .cpp file added

---
+++ chatRoom.cpp
@@ -0,0 +1,38 @@
+#include "chatroom.hpp"
+
+void Room::join(ParticipantPointer participant) {
+    this->participants.insert(participant);
+}
+
+void Room::leave(ParticipantPointer participant) {
+    this->participants.erase(participant);
+}
+
+void Room::deliver(ParticipantPointer participant, Message& message) {
+    messageQueue.push_back(message);
+
+    while(!messageQueue.empty()) {
+        Message msg = messageQueue.front();
+        messageQueue.pop_front();
+
+        for(ParticipantPointer _participant: participants) {
+            if(_participant != participant) {
+                _participant.write(msg);
+            }
+        }
+    }
+}
+
+void Session::write(Message& message) {
+    messageQueue.push_back(message);
+
+    while(!messageQueue.empty()) {
+        Message msg = messageQueue.front();
+        messageQueue.pop_front();
+
+        bool header_decode = message.decodeHeader();
+        if(header_decode) {
+            std::string body = message.getBody();
+        }
+    }
+}
